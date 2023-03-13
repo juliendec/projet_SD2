c316e1031b5765ffce95251b2a076aa742dca7f1
juliendec <juliendecoen2308@gmail.com>
2023-03-13 11:50:22 +0100

    feat:add equals to Station

--- src/Station.java
+++ src/Station.java
@@ -1,7 +1,26 @@
+import java.util.Objects;
+
 public class Station {
   private String nomStation;
 
   public Station(String nomStation) {
     this.nomStation = nomStation;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Station station = (Station) o;
+    return Objects.equals(nomStation, station.nomStation);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(nomStation);
   }
 }
